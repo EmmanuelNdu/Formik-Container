5fa1efaacc39ca87a8d41d2e5844c79fcc33acd2
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2025-01-10 02:48:00 +0100

    lol

--- src/components/FormikContainer.jsx
+++ src/components/FormikContainer.jsx
@@ -10,15 +10,23 @@ const FormikContainer = () => {
     {key: 'option 2', value:'option2'},
     {key: 'option 3', value:'option3'}
   ]
+  const radioOptions = [
+   { key: 'Option 1', value:'rOption1'},
+   { key: 'Option 2', value:'rOption2'},
+   { key: 'Option 3', value:'rOption3'}
+  ]
+
     const initialValues = {
       email: '',
       description: '',
-      selectOption: ''
+      selectOption: '',
+      radioOptions: ''
     }
     const validationSchema = Yup.object({
       email: Yup.string().required('Required'),
       description: Yup.string().required('Required'),
-      selectOption: Yup.string().required('Required')
+      selectOption: Yup.string().required('Required'),
+      radioOption: Yup.string().required('Required')
     })
     const onsubmit = values => console.log('Form data', values)
   return (
@@ -45,6 +53,12 @@ const FormikContainer = () => {
               label='Select a topic'
               name='selectOption'
               options={dropdownOptions}
+              />
+              <FormikControl
+              control=''radio
+              label='Radio Topic'
+              name='radioOption'
+              options={radioOptions}
               />
                 <button type='submit'>Submit</button>
             </Form>
--- src/components/RadioButtons.jsx
+++ src/components/RadioButtons.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Field, ErrorMessage } from 'formik'
 import TextError from './TextError'
 
-const RadioButtons = () => {
+const RadioButtons = (props) => {
     const { label, name, options, ...rest } = props
   return (
     <div className='form-control'>
@@ -12,18 +12,22 @@ const RadioButtons = () => {
                 ({field}) => {
                     return options.map(option => {
                         return (
-                            <React.Fragment>
+                            <React.Fragment key={option.key}>
                                 <input 
                                 type='radio' 
                                 id={option.value} 
-                                {...field} value={option.value} 
-                                checked={field.value === option.value}/>
+                                {...field} 
+                                value={option.value} 
+                                checked={field.value === option.value}
+                                />
+                                <label htmlFor={option.value}>{option.key}</label>
                             </React.Fragment>
                         )
                     })
                 }
             }
         </Field>
+        <ErrorMessage name={name} component={TextError} />
     </div>
   )
 }
